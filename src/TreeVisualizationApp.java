35858a2f8e47b597ddf1b01c7f4b13c888844372
davinchi73 <dev107c.d133@gmail.com>
2022-12-02 14:38:58 -0600

    commit

--- src/TreeVisualizationApp.java
+++ src/TreeVisualizationApp.java
@@ -3,16 +3,25 @@
 import edu.macalester.graphics.CanvasWindow;
 
 public class TreeVisualizationApp {
-    private final CanvasWindow canvas;
     private static final int CANVAS_HEIGHT = 800;
     private static final int CANVAS_WIDTH = 1000;
 
-    public TreeVisualizationApp(int width, int height) {
-        canvas = new CanvasWindow("Tree Visualization App", width, height);
+    public TreeVisualizationApp() {
     }
 
     public static void main(String[] args) {
-        TreeVisualizationApp treeApp = new TreeVisualizationApp(CANVAS_WIDTH, CANVAS_HEIGHT);
+        TreeVisualizationApp newApp = new TreeVisualizationApp();
+        treeAppRunner(newApp);
+    }
+
+    /**
+     * Method which will create a canvas, and implement a user interface 
+     * which will allow a user to see implmentations for AVL and Black and Red trees.
+     * @param newApp
+     */
+    public static void treeAppRunner(TreeVisualizationApp newApp) {
+        CanvasWindow canvas = new CanvasWindow("Tree Visualization App", CANVAS_WIDTH, CANVAS_HEIGHT);
+        
     }
 }
 
