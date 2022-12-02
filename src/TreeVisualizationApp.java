73a641c941adc837473c6321a7033134c5a4b936
BraydenCoronado <bcoronad@macalester.edu>
2022-12-02 14:31:13 -0600

    wider

--- src/TreeVisualizationApp.java
+++ src/TreeVisualizationApp.java
@@ -5,7 +5,7 @@ import edu.macalester.graphics.CanvasWindow;
 public class TreeVisualizationApp {
     private final CanvasWindow canvas;
     private static final int CANVAS_HEIGHT = 800;
-    private static final int CANVAS_WIDTH = 800;
+    private static final int CANVAS_WIDTH = 1000;
 
     public TreeVisualizationApp(int width, int height) {
         canvas = new CanvasWindow("Tree Visualization App", width, height);
