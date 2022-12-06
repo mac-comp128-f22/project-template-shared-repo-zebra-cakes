b7448fafa671e60b220ab5c41a1b7d532c72624d
Minh Nguyen <98056414+MikeNguyn@users.noreply.github.com>
2022-12-05 21:09:16 -0600

    Completed positions

--- src/BSTTreeVisualization.java
+++ src/BSTTreeVisualization.java
@@ -1,29 +1,36 @@
+import java.lang.System.Logger.Level;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import edu.macalester.graphics.*;
 
-public class BSTTreeVisualization<E> extends BinarySearchTree<E> {
+public class BSTTreeVisualization<E extends Comparable<E>> extends BinarySearchTree<E> {
     public GraphicsGroup BSTGraphics;
-    public int width;
-    public int height;
-    public HashMap<Integer, Point> coordinate;
+    public double width;
+    public double height;
+    public HashMap<Integer, Point> coordinate = new HashMap<>();
 
-    public BSTTreeVisualization(int width, int height) {
+    public BSTTreeVisualization(double width, double height) {
         super();
         this.width = width;
         this.height = height;
+        createCoordinates();
     }
     
     public void createCoordinates() {
-        int verticalGap = this.height / 6;
         for (int i = 0; i <= 31; i++) {
-            int level = Math 
+            double level = Math.floor(Math.log(i + 1) / Math.log(2));
+            double verticalGap = (level + 1) * this.height / 6;
+            double startingNode = Math.pow(2, level) - 1;
+            double numberOfNodes = Math.pow(2, level);
+            double horizontalGap = (i - startingNode + 1) * (this.width / (numberOfNodes + 1));
+            coordinate.put(i, new Point(horizontalGap, verticalGap));
         }
     }
-
-
-
-    
-    
+    public static void main(String[] args) {
+        CanvasWindow canvas = new CanvasWindow("test", 400, 200);
+        BSTTreeVisualization<Integer> test = new BSTTreeVisualization<>(canvas.getWidth(), canvas.getHeight());
+        test.coordinate.forEach((k, v) -> canvas.add(new Ellipse(v.getX(), v.getY(), 1, 1)));      
+    }
 }
